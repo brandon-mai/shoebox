34bda3e6b41878ea2b77c6344361fced16029be6
Brandon Mai <61886250+brandon-mai@users.noreply.github.com>
2026-02-25 00:18:28 +0700

    feat: annual color theme overhaul

--- src/components/bento/WakatimeGraph.tsx
+++ src/components/bento/WakatimeGraph.tsx
@@ -22,13 +22,13 @@ interface Props {
 }
 
 const colors = [
+  'var(--chart-1)',
   'var(--chart-2)',
-  'var(--chart-1)',
-  'var(--chart-1)',
-  'var(--chart-1)',
   'var(--chart-3)',
-  'var(--chart-3)',
-  'var(--chart-3)',
+  'var(--chart-4)',
+  'var(--chart-5)',
+  'var(--chart-5)',
+  'var(--chart-5)',
 ]
 
 const chartConfig: ChartConfig = {
